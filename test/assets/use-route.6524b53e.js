66b6a7945e7017d19a0f5f11ee0549add253ac2b
chenzhilin <782978870@qq.com>
2021-10-13 03:04:31 +0800

    4

--- test/assets/use-route.6524b53e.js
+++
@@ -1,2 +0,0 @@
-import{l as r}from"./vendor.64c8d20c.js";var o={to:[String,Object],url:String,replace:Boolean};function a(r){var o=r.$router,{to:a,url:e,replace:t}=r;a&&o?o[t?"replace":"push"](a):e&&(t?location.replace(e):location.href=e)}function e(){var o=r().proxy;return()=>a(o)}export{a,o as r,e as u};
-//# sourceMappingURL=use-route.6524b53e.js.map
