c43ef1fbe59dd8495e63f53a34ebd3091a4969a6
chenzhilin <782978870@qq.com>
2021-10-13 03:02:13 +0800

    3

---
+++ test/assets/Index.4f024e89.js
@@ -0,0 +1,2 @@
+import{r,o as e,c as n}from"./vendor.64c8d20c.js";const o={name:"Index"};o.render=function(o,t,c,s,a,d){const u=r("router-view");return e(),n(u)};export default o;
+//# sourceMappingURL=Index.4f024e89.js.map
